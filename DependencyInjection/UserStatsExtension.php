65210a2ebf67cd78c74ecc6aac3431a692b496ad
devcut <guillaume.egloff@gmail.com>
2020-10-05 15:01:36 +0200

    Dependency config

---
+++ DependencyInjection/UserStatsExtension.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace FluffyFactory\Bundle\UserStatsBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class UserStatsExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yaml');
+
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $container->setParameter('fluffy_user_stats.entity', $config['entity']);
+    }
+}

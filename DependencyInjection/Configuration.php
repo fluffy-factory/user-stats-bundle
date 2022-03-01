a27fc05ee5f3b27a2d94054c4c23b3752c37e1aa
devcut <guillaume.egloff@gmail.com>
2022-03-01 14:00:43 +0100

    Add Configuration

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace FluffyFactory\Bundle\UserStatsBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder('fluffy_user_stats');
+
+        $treeBuilder->getRootNode()
+            ->children()
+                ->arrayNode('exclude_route')->end()
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
